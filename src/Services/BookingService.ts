661e489cc2c5ece43a19b0d5678ee781af831a9a
sara <sara.ekmanMedieinstitutet@gmail.com>
2024-02-13 18:24:27 +0100

    :sparkles: Complete BookingForm component with type-safe inputs and customer details

--- src/Services/BookingService.ts
+++ src/Services/BookingService.ts
@@ -1,3 +1,4 @@
+import { Booking } from "../models/Booking"
 import { get, post } from "./serviceBase"
 
 const API_BASE_URL = "https://school-restaurant-api.azurewebsites.net/"
@@ -12,10 +13,15 @@ export const createRestaurant = async () => {
   }
     }
     )
-    console.log(response)
+    console.log(response.data)
 }
 
 export const getBookings = async () => {
-  const response = await get(`${API_BASE_URL}booking/restaurant/65cb4a68505ba22f8dc6636f`)
- console.log(response)
+  const response = await get<Booking[]>(`${API_BASE_URL}booking/restaurant/65cb4a68505ba22f8dc6636f`)
+ return response.data
+}
+
+export const createBooking = async (newBooking: Booking) => {
+    const response = await post<Booking>(`${API_BASE_URL}booking/create`, newBooking)
+    console.log(response.data)
 }
