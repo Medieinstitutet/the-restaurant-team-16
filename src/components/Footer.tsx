dce288e531d846db34261f77f37c7a0cfee5b0a3
sara <sara.ekmanMedieinstitutet@gmail.com>
2024-02-21 00:18:38 +0100

    :art: :lipstick: add responsive design for Footer & improve the code structure and add more info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,32 @@
 const Footer = () => {
   return (
-    <>
-      <div>
-        <p>Opening Hours</p>
-        <p>Monday - Friday: 10:00 - 22:00</p>
-        <p>Saturday - Sunday: 12:00 - 22:00</p>
-      </div>
-      <div>
-        <p>Address</p>
-        <p>1234 Golden Fork Street</p>
-        <p>123 45 Golden City</p>
-      </div>
-      <div>
-        <p>Contact</p>
-        <p>Phone: 123 456 789</p>
-        <p>Email: example@email.com</p>
-      </div>
-    </>
+    <section className="footer_container">
+      <article className="footer-info">
+        <h3>The Golden Fork</h3>
+        <p>123 Delicious Street, Tasty City, Yum 45678</p>
+        <p>+46 123 456 789 | info@thegoldenfork.com</p>
+        <p> Open daily from 11:00 AM to 11:00 PM.</p>
+      </article>
+      <article className="footer_nav">
+        <h4>Navigation</h4>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/booking-page">Reservations</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </article>
+      <article className="footer_social">
+        <h4>Follow Us</h4>
+        <a href="#">Instagram</a> | <a href="#">Facebook</a> |{" "}
+        <a href="#">Twitter</a>
+      </article>
+    </section>
   );
 };
 
