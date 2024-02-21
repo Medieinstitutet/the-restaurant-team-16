a297540f8545cb88c4af2b2f3000c63f01f44ff2
sara <sara.ekmanMedieinstitutet@gmail.com>
2024-02-21 19:46:49 +0100

    :art: :lipstick: add responsive design for Admin & bookings & Improve structure / format of the code.

--- src/components/Bookings.tsx
+++ src/components/Bookings.tsx
@@ -3,7 +3,6 @@ import { IBooking } from "../models/IBooking";
 import { useBookings } from "../contexts/BookingsContext";
 import { ActionType } from "../reducers/BookingReducer";
 import { deleteBooking, updateBooking } from "../Services/BookingService";
-
 export interface IBookingProps {
   booking: IBooking;
 }
@@ -38,38 +37,50 @@ const Bookings = ({ booking }: IBookingProps) => {
   };
 
   return (
-    <div className="bookingInputs">
-      <label htmlFor="id">Booking ID</label>
+    <div className="booking_content">
+      <label htmlFor="id" className="form__label">
+        Booking ID
+      </label>
       <input
         type="text"
         name="id"
         value={booking._id}
         onChange={handleChange}
         disabled={!editing}
+        className="form__input"
       />
-      <label htmlFor="date">Date</label>
+      <label htmlFor="date" className="form__label">
+        Date
+      </label>
       <input
         type="date"
         name="date"
         value={editedBooking.date}
         onChange={handleChange}
         disabled={!editing}
+        className="form__input"
       />
-      <label htmlFor="numberOfGuests">Guests</label>
+      <label htmlFor="numberOfGuests" className="form__label">
+        Guests
+      </label>
       <input
         type="number"
         name="numberOfGuests"
         value={editedBooking.numberOfGuests}
         onChange={handleChange}
         disabled={!editing}
+        className="form__input"
       />
-      <label htmlFor="time">Time</label>
+      <label htmlFor="time" className="form__label">
+        Time
+      </label>
       <input
         type="time"
         name="time"
         value={editedBooking.time}
         onChange={handleChange}
         disabled={!editing}
+        className="form__input"
       />
       <div>
         <button onClick={handleEdit}>
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,63 +1,77 @@
-import { createBooking, getCustomersId } from '../Services/BookingService';
-import Bookings from '../components/Bookings';
-import { useBookings } from '../contexts/BookingsContext';
-import { ActionType } from '../reducers/BookingReducer';
-import Button, { ITheme } from '../components/Button';
-import { BookingForm } from '../components/BookingForm';
-import { useState } from 'react';
-import { Booking } from '../models/Booking';
-import { Message, MessageType } from '../components/Message';
+import { createBooking, getCustomersId } from "../Services/BookingService";
+import Bookings from "../components/Bookings";
+import { useBookings } from "../contexts/BookingsContext";
+import { ActionType } from "../reducers/BookingReducer";
+import Button, { ITheme } from "../components/Button";
+import { BookingForm } from "../components/BookingForm";
+import { useState } from "react";
+import { Booking } from "../models/Booking";
+import { Message, MessageType } from "../components/Message";
+import "../styles/_admin.scss";
 
 const Admin = () => {
   const { bookings, dispatch } = useBookings();
   const [showForm, setShowForm] = useState(false);
-   const [message, setMessage] = useState<{ text: string, type: MessageType } | null>(null);
+  const [message, setMessage] = useState<{
+    text: string;
+    type: MessageType;
+  } | null>(null);
   const [showMessage, setShowMessage] = useState(false);
 
   const handleClick = () => {
-    setShowForm(!showForm)
-  }
+    setShowForm(!showForm);
+  };
 
-   const setTimeOutMessage = () => {
-     setTimeout(() => {
-          setMessage(null);
-          setShowMessage(false);
-        
-        }, 3000);
+  const setTimeOutMessage = () => {
+    setTimeout(() => {
+      setMessage(null);
+      setShowMessage(false);
+    }, 3000);
+  };
+
+  const addNewBooking = async (newBooking: Booking) => {
+    try {
+      const createdBooking = await createBooking(newBooking);
+      getCustomersId(createdBooking.insertedId).then(data => {
+        dispatch({ type: ActionType.ADD, payload: data });
+      });
+      setShowForm(false);
+      setShowMessage(true);
+      setMessage({
+        text: `Thank ${newBooking.customer.name} ${newBooking.customer
+          .lastname} for your booking`,
+        type: MessageType.SUCCES
+      });
+      setTimeOutMessage();
+    } catch (error) {
+      console.error("Failed to create booking:", error);
     }
-
-    const addNewBooking = async (newBooking: Booking) => {
-        try {
-            const createdBooking = await createBooking(newBooking);
-          getCustomersId(createdBooking.insertedId).then((data) => {
-            dispatch({ type: ActionType.ADD, payload: data });
-          });
-          setShowForm(false);
-          setShowMessage(true);
-          setMessage({ text: `Thank ${newBooking.customer.name} ${newBooking.customer.lastname} for your booking`, type: MessageType.SUCCES });
-          setTimeOutMessage();
-
-        } catch (error) {
-            console.error('Failed to create booking:', error);
-        }
-  }
+  };
 
   return (
-    <>
+    <div className="admin_wrapper">
       <h1>Bookings</h1>
-      <Button text={`${showForm ? 'Hide the form' :'Add new booking'}`} theme={ITheme.PRIMARY} handleClick={handleClick}>
-      </Button>
+      <Button
+        text={`${showForm ? "Hide the form" : "Add new booking"}`}
+        theme={ITheme.SECONDARY}
+        handleClick={handleClick}
+      />
 
-      {showForm ? <div><BookingForm handleClick={addNewBooking} /></div> : <div>
-        {showMessage && <Message text={message!.text} type={message!.type} />}
-            </div>}
+      {showForm
+        ? <div>
+            <BookingForm handleClick={addNewBooking} />
+          </div>
+        : <div>
+            {showMessage &&
+              <Message text={message!.text} type={message!.type} />}
+          </div>}
 
-      <ul className="bookings">
-        {bookings.map((booking, index) => (
+      <div className="bookings_container">
+        {bookings.map((booking, index) =>
           <Bookings key={index} booking={booking} />
-        ))}
-      </ul>
-    </>
+        )}
+      </div>
+    </div>
   );
 };
 
